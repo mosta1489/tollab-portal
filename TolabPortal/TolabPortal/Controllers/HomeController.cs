b28ec6c38a7cdd77be0fe8ea26417fd72a48a671
Hussein Mosaad <7essein.cis@gmail.com>
2021-10-22 15:35:35 +0200

    add login page

--- TolabPortal/TolabPortal/Controllers/HomeController.cs
+++ TolabPortal/TolabPortal/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using TolabPortal.Models;
 
 namespace TolabPortal.Controllers
 {
@@ -23,15 +17,10 @@ namespace TolabPortal.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [Route("~/login")]
+        public IActionResult Login()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
