4ebe1287df4f3a9a522fc610e3874da48de0870b
Hussein Mosaad <7essein.cis@gmail.com>
2021-10-30 03:25:34 +0200

    too many fixes

--- TolabPortal/Tolab.Common/ApplicationConfig.cs
+++ TolabPortal/Tolab.Common/ApplicationConfig.cs
@@ -5,5 +5,8 @@
         public string ApiUrl { get; set; }
         public string MediaBaseUrl { get; set; }
         public string ZoomApiKey { get; set; }
+        public string MyFatoorahApiKey { get; set; }
+        public string MyFatooraApiUrl { get; set; }
+        public string CallBackPayemntRoot { get; set; }
     }
 }
--- TolabPortal/TolabPortal/Controllers/MyFatoorahPaymentController.cs
+++ TolabPortal/TolabPortal/Controllers/MyFatoorahPaymentController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Tolab.Common;
 using TolabPortal.DataAccess.Models.Payment;
@@ -15,78 +13,71 @@ using TolabPortal.ViewModels;
 
 namespace TolabPortal.Controllers
 {
- 
     public class MyFatoorahPaymentController : Controller
     {
         private readonly IMyFatoorahPaymentService _paymentService;
         private readonly ISubscribeService _subscribeService;
         private readonly ISessionManager _sessionManager;
         private readonly ILogger<MyFatoorahPaymentController> _logger;
-        private readonly IConfiguration _config;
+        private readonly ApplicationConfig _config;
 
         public MyFatoorahPaymentController(IMyFatoorahPaymentService paymentService, ISubscribeService subscribeService,
-            ISessionManager sessionManager, ILogger<MyFatoorahPaymentController> logger,IConfiguration config)
+            ISessionManager sessionManager, ILogger<MyFatoorahPaymentController> logger, IOptions<ApplicationConfig> config)
         {
             _paymentService = paymentService;
             this._subscribeService = subscribeService;
             this._sessionManager = sessionManager;
             this._logger = logger;
-            this._config = config;
+            this._config = config.Value;
         }
 
-
-         
-       // [Route("~/InitiatePayment/{amount}")]
-       [HttpPost]
+        // [Route("~/InitiatePayment/{amount}")]
+        [HttpPost]
         public async Task<IActionResult> InitiatePayment(PayVm payViewmodel)
         {
-
             var response = await _paymentService.InitiatePayment(new InitiatePaymentRequest
             {
                 InvoiceAmount = payViewmodel.InvoiceAmount,
                 CurrencyIso = "kwd"
-
-            }).ConfigureAwait(false); 
+            }).ConfigureAwait(false);
             if (response.IsSuccess)
-                return View(new PaymentViewModel() {
-                    InvoiceValue= payViewmodel.InvoiceAmount,
-                    PaymentMethods=response.Data.PaymentMethods,
-                    CustomerName=_sessionManager.UserId??"",
+                return View(new PaymentViewModel()
+                {
+                    InvoiceValue = payViewmodel.InvoiceAmount,
+                    PaymentMethods = response.Data.PaymentMethods,
+                    CustomerName = _sessionManager.UserId ?? "",
                     CustomerReference = payViewmodel.TransactionId,
-                    TransactionType=payViewmodel.TransactionType,
-                    ReturnUrl = $"{_config["CallBackPayemntRoot"]}{payViewmodel.ReturnRoute}"
+                    TransactionType = payViewmodel.TransactionType,
+                    ReturnUrl = $"{_config.CallBackPayemntRoot}{payViewmodel.ReturnRoute}"
                 });
             return View("ErrorPayment");
         }
 
- 
         [HttpPost("executePayment")]
         public async Task<IActionResult> ExecutePayment(PaymentViewModel paymentVm)
         {
             if (ModelState.IsValid)
             {
-                var response = await _paymentService.ExecutePayment(new ExecutePaymentRequest() {
+                var response = await _paymentService.ExecutePayment(new ExecutePaymentRequest()
+                {
                     PaymentMethodId = paymentVm.PaymentMethodId,
                     InvoiceValue = paymentVm.InvoiceValue,
                     CustomerReference = paymentVm.CustomerReference,
                     CustomerName = _sessionManager.UserId ?? "",
                     UserDefinedField = $"{paymentVm.TransactionType},{paymentVm.ReturnUrl}",
-                    CallBackUrl = $"{_config["CallBackPayemntRoot"]}/CompletePayment",
-                    ErrorUrl = $"{_config["CallBackPayemntRoot"]}/ErrorPayment",
+                    CallBackUrl = $"{_config.CallBackPayemntRoot}/CompletePayment",
+                    ErrorUrl = $"{_config.CallBackPayemntRoot}/ErrorPayment",
                     Language = "AR",
                     ExpiryDate = DateTime.Now.AddYears(1)
                 }).ConfigureAwait(false);
                 if (response.IsSuccess)
                 {
-                   
                     return Redirect(response.Data.PaymentURL);
                 }
-                
             }
             return View("ErrorPayment");
-
-
         }
+
         [AllowAnonymous]
         [Route("~/ErrorPayment")]
         public IActionResult ErrorPayment()
@@ -94,12 +85,9 @@ namespace TolabPortal.Controllers
             return View();
         }
 
-
-
         [Route("~/CompletePayment")]
         public async Task<IActionResult> CompletePayment(string paymentId)
         {
-
             try
             {
                 var message = await _paymentService.LogTransaction(new GetPaymentStatusRequest
@@ -116,11 +104,13 @@ namespace TolabPortal.Controllers
                         switch (computedFiled[0])
                         {
                             case string transaction when int.Parse(transaction) == (int)TransactionType.Course:
-                                await _subscribeService.SubscribeCourse(message,!string.IsNullOrEmpty(response.Data.CustomerReference) ? long.Parse(response.Data.CustomerReference) : 0);
+                                await _subscribeService.SubscribeCourse(message, !string.IsNullOrEmpty(response.Data.CustomerReference) ? long.Parse(response.Data.CustomerReference) : 0);
                                 break;
+
                             case string transaction when int.Parse(transaction) == (int)TransactionType.Live:
-                                await _subscribeService.SubscribeLive(message,!string.IsNullOrEmpty(response.Data.CustomerReference) ? long.Parse(response.Data.CustomerReference) : 0);
+                                await _subscribeService.SubscribeLive(message, !string.IsNullOrEmpty(response.Data.CustomerReference) ? long.Parse(response.Data.CustomerReference) : 0);
                                 break;
+
                             case string transaction when int.Parse(transaction) == (int)TransactionType.Track:
                                 await _subscribeService.SubscribeTrack(!string.IsNullOrEmpty(response.Data.CustomerReference) ? long.Parse(response.Data.CustomerReference) : 0);
                                 break;
@@ -129,25 +119,16 @@ namespace TolabPortal.Controllers
                         if (!string.IsNullOrEmpty(computedFiled[1]))
                             return Redirect(computedFiled[1]);
                         return RedirectToAction("Index", "Home");
-
                     }
-
-
                 }
 
                 return View("ErrorPayment");
-
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Complete payment :{ex.Message} ||StackTrace : {ex.StackTrace} ");
                 return View("ErrorPayment");
             }
-
         }
-
-
-
-         
     }
 }
