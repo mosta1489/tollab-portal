b4a63ea726bc0e43331327fef5f4b51956abe537
Hussein Mosaad <7essein.cis@gmail.com>
2021-11-07 01:55:27 +0200

    fix

--- TolabPortal/TolabPortal/Infrastructure/InterestsMiddleware.cs
+++ TolabPortal/TolabPortal/Infrastructure/InterestsMiddleware.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
-using System;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Tolab.Common;
 using TolabPortal.DataAccess.Models;
@@ -21,36 +21,33 @@ namespace TolabPortal.Infrastructure
 
         public async Task InvokeAsync(HttpContext context, IAccountService accountService)
         {
-            try
+            if (context.User.Identity != null && context.User.Identity.IsAuthenticated)
             {
-                if (context.User.Identity != null && context.User.Identity.IsAuthenticated)
+                var studentProfileResponse = await accountService.GetStudentProfile();
+                if (studentProfileResponse.StatusCode == HttpStatusCode.Unauthorized)
                 {
-                    var studentProfileResponse = await accountService.GetStudentProfile();
-                    var studentProfile =
-                        await CommonUtilities.GetResponseModelFromJson<StudentResponse>(studentProfileResponse);
-
-                    if (!studentProfile.Student.Interests.Any() && context.Request.Path.Value != null
-                                                                && !context.Request.Path.Value.Contains("/Interest")
-                                                                && !context.Request.Path.Value.Contains("/logout"))
+                    await context.Request.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                    var logoutResponse = await accountService.LogoutStudent();
+                    if (logoutResponse.IsSuccessStatusCode)
                     {
-                        context.Response.Redirect("/Interest/RegisterSection");
-                        return;
+                        var logoutResult = await CommonUtilities.GetResponseModelFromJson<StudentLogoutResponse>(logoutResponse);
                     }
+                    context.Response.Redirect("/login");
+                    return;
                 }
+                var studentProfile =
+                    await CommonUtilities.GetResponseModelFromJson<StudentResponse>(studentProfileResponse);
 
-                await _next(context);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                await context.Request.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-                var logoutResponse = await accountService.LogoutStudent();
-                if (logoutResponse.IsSuccessStatusCode)
+                if (!studentProfile.Student.Interests.Any() && context.Request.Path.Value != null
+                                                            && !context.Request.Path.Value.Contains("/Interest")
+                                                            && !context.Request.Path.Value.Contains("/logout"))
                 {
-                    var logoutResult = await CommonUtilities.GetResponseModelFromJson<StudentLogoutResponse>(logoutResponse);
+                    context.Response.Redirect("/Interest/RegisterSection");
+                    return;
                 }
-                context.Response.Redirect("/login");
-                return;
             }
+
+            await _next(context);
         }
 
         private bool IsPathContainsValue(HttpContext httpContext, string value)
