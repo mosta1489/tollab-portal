42db8c7c41642dd1405fa886167f64881b6a794f
Ahmed nasser <Ahmed nasser@DESKTOP-7TLDKLI>
2021-11-07 01:49:18 +0300

    adding vimeo connector

---
+++ TolabPortal/TolabPortal/Controllers/Utils/VimeoConnector.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TolabPortal.Controllers.Utils
+{
+    public static class VimeoConnector
+    {
+        private  const string VimeoToken = "3251e9876001f9b94a96b446bacdb6c2";
+        public static async Task<string> GenerateIntroEmbed(string introVideoUrl)
+        {
+
+            try
+            {
+                var client = new HttpClient();
+                string uri = $"https://vimeo.com/api/oembed.json?url={introVideoUrl}&width=400&height=300";
+                client.DefaultRequestHeaders.Authorization
+                = new AuthenticationHeaderValue("Bearer", VimeoToken);
+                client.DefaultRequestHeaders.Add("Accept", "application/vnd.vimeo.*+json;version=3.4");
+                var response = await client.GetAsync(uri);
+                var responseString = await response.Content.ReadAsStringAsync();
+                var serialized = JsonConvert.DeserializeObject<VimeoResponse>(responseString);
+                return serialized.html ?? "";
+               
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+
+
+        }
+    }
+}
