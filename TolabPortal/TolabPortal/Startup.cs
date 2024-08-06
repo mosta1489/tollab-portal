e5ee58efab86c07425070dc2da027c6d952ab0f7
Saleh Ibrahim <SalehIbrahim@FortTeck.com>
2024-08-07 00:53:52 +0300

    remove 'UseHttpsRedirection' middleware.

--- TolabPortal/TolabPortal/Startup.cs
+++ TolabPortal/TolabPortal/Startup.cs
@@ -70,7 +70,7 @@ namespace TolabPortal
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
             app.UseWebOptimizer();
